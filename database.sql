3f78f206bfb01d613ed223139011e39dffd2b5e0
Hansadi Kumanayake <hansadi.kumanayake@gmail.com>
2026-07-06 16:33:35 +0530

    Add the attendance_events table into the database

--- database.sql
+++ database.sql
@@ -6,3 +6,15 @@ CREATE TABLE users (
     role ENUM('user', 'admin') NOT NULL
 );
 
+CREATE TABLE attendance_events (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    action_type ENUM('IN', 'OUT') NOT NULL,
+    latitude DECIMAL(10,8) NOT NULL,
+    longitude DECIMAL(11,8) NOT NULL,
+    photo_path VARCHAR(255),
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
