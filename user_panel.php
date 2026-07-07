d57ec5edf3dd8e6bb43370fb3b711d8005024269
Your Name <your_email@example.com>
2026-07-07 12:53:08 +0530

    add new changes

--- user_panel.php
+++ user_panel.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FieldTrack</title>
 
-    <link rel="stylesheet" href="fieldtrack_style.css">
+    <link rel="stylesheet" href="user_panel.css">
 </head>
 <body>
 
