a703a81a67bc34b3012aacce02a66a7a9d6be7a2
Hansadi Kumanayake <hansadi.kumanayake@gmail.com>
2026-07-07 10:04:13 +0530

    Add database connection file

--- db.php
+++ db.php
@@ -7,7 +7,7 @@ $database="fieldtrack_db";
 $conn=mysqli_connect($servername,$username,$password,$database);
 
 if(!conn){
-    
+    die("Database connection failed: ".mysqli_connect_error());
 }
 
 ?>
