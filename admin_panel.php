9f08b50979efd91d78131f232720ecd720179f9d
Hansadi Kumanayake <hansadi.kumanayake@gmail.com>
2026-07-07 16:49:21 +0530

    Include Leaflet stylesheet for admin map

--- admin_panel.php
+++ admin_panel.php
@@ -50,6 +50,7 @@ if (!$records_result) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FieldTrack Admin Dashboard</title>
     <link rel="stylesheet" href="admin_style.css">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css">
 </head>
 
 <body>
