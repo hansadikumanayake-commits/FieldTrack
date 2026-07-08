8e332f65cda921304549e821945d7ed38cfb11f1
Hansadi Kumanayake <hansadi.kumanayake@gmail.com>
2026-07-08 14:14:41 +0530

    updates are added

--- login_process.php
+++ login_process.php
@@ -26,7 +26,8 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
         }
 
     }else{
-        echo "Invalid username or password";
+        header("Location: login_failed.php");
+        exit();
     }
 }
 else{
