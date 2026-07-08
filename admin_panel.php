566c6d69b1bbdf321166607a39287853e95fbffb
Hansadi Kumanayake <hansadi.kumanayake@gmail.com>
2026-07-08 12:38:32 +0530

    Add the admin panel protection

--- admin_panel.php
+++ admin_panel.php
@@ -1,4 +1,11 @@
 <?php
+
+session_start();
+if(!isset($_SESSION['user_id']) || $_SESSION['role']!= 'admin'){
+    header("Location:login.php");
+    exit();
+}
+
 include 'db.php';
 
 // Total number of officers
