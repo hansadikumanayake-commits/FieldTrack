ce53cd1fd0841a8eb4eb8994e601adf5e9ed207d
Hansadi Kumanayake <hansadi.kumanayake@gmail.com>
2026-07-08 14:13:20 +0530

    Add the login failed file just incase if any login fails

---
+++ login_failed.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <title>Login Failed-FieldTrack</title>
+        <link rel="stylesheet" href="login_style.css">
+    </head>
+
+    <body>
+        <div class="login-container">
+            <div class="login-box error-box">
+                <h1>Login Failed</h1>
+                <p>The username or password you entered is incorrect</p>
+
+                <div class="error-message">
+                    Please check your login details and try again
+                </div>
+
+                <a href="login.php" class="login-again-btn">Go Back to Login</a>
+                
+            </div>
+        </div>
+    </body>
+
+</html>
