cbefb0f1db13b0d3c2175d2f2c9652e7968a2eca
Hansadi Kumanayake <hansadi.kumanayake@gmail.com>
2026-07-08 14:53:49 +0530

    Upload the updates

--- login_process.php
+++ login_process.php
@@ -2,37 +2,41 @@
 session_start();
 include 'db.php';
 
-if($_SERVER['REQUEST_METHOD']=="POST"){
-    $username=$_POST['username'];
-    $password=$_POST['password'];
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
-    $sql="SELECT * FROM users WHERE username='$username' AND password='password' ";
-    $result=mysqli_query($conn,$sql);
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-    if(mysqli_num_rows($result)==1){
-        $user=mysqli_fetch_assoc($result);
+    $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+    $result = mysqli_query($conn, $sql);
 
-        $_SESSION['user_id']=$user['id'];
-        $_SESSION['name']=$user['name'];
-        $_SESSION['role']=$user['role'];
+    if (!$result) {
+        die("Query failed: " . mysqli_error($conn));
+    }
 
-        if($user['role']=='admin'){
-            header("Location:admin_panel.php");
+    if (mysqli_num_rows($result) == 1) {
+
+        $user = mysqli_fetch_assoc($result);
+
+        $_SESSION['user_id'] = $user['id'];
+        $_SESSION['name'] = $user['name'];
+        $_SESSION['role'] = $user['role'];
+
+        if ($user['role'] == 'admin') {
+            header("Location: admin_panel.php");
+            exit();
+        } else {
+            header("Location: user_panel.php");
             exit();
         }
-        else{
-            header("Location:user_panel.php");
-            exit();
-        }
 
-    }else{
+    } else {
         header("Location: login_failed.php");
         exit();
     }
-}
-else{
-    header ("Location:login.php");
+
+} else {
+    header("Location: login.php");
     exit();
 }
-
 ?>
