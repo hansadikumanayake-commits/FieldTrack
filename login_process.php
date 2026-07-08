bb4b874c62bfd1039beaad1578961656653b9dad
Hansadi Kumanayake <hansadi.kumanayake@gmail.com>
2026-07-08 12:17:45 +0530

    Add login processing with role based redirection

---
+++ login_process.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+include 'db.php';
+
+if($_SERVER['REQUEST_METHOD']=="POST"){
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+
+    $sql="SELECT * FROM users WHERE username='$username' AND password='password' ";
+    $result=mysqli_query($conn,$sql);
+
+    if(mysqli_num_rows($result)==1){
+        $user=mysqli_fetch_assoc($result);
+
+        $_SESSION['user_id']=$user['id'];
+        $_SESSION['name']=$user['name'];
+        $_SESSION['role']=$user['role'];
+
+        if($user['role']=='admin'){
+            header("Location:admin_panel.php");
+            exit();
+        }
+        else{
+            header("Location:user_panel.php");
+            exit();
+        }
+
+    }else{
+        echo "Invalid username or password";
+    }
+}
+else{
+    header ("Location:login.php");
+    exit();
+}
+
+?>
