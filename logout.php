1775a4dbaaa826395d695f1bc76a65fff5b65496
Hansadi Kumanayake <hansadi.kumanayake@gmail.com>
2026-07-16 16:07:27 +0530

    Secure logout by clearing session data and cookies

--- logout.php
+++ logout.php
@@ -1,10 +1,39 @@
 <?php
 
-session_start();
+declare(strict_types=1);
 
+require_once 'auth.php';
+
+/*
+ * Remove all session values.
+ */
 $_SESSION = [];
 
+/*
+ * Delete the session cookie from the browser.
+ */
+if (ini_get('session.use_cookies')) {
+    $cookieParameters =
+        session_get_cookie_params();
+
+    setcookie(
+        session_name(),
+        '',
+        time() - 42000,
+        $cookieParameters['path'],
+        $cookieParameters['domain'],
+        $cookieParameters['secure'],
+        $cookieParameters['httponly']
+    );
+}
+
+/*
+ * Destroy the server-side session.
+ */
 session_destroy();
 
-header("Location: login.php");
+/*
+ * Return the user to the login page.
+ */
+header('Location: login.php');
 exit();
